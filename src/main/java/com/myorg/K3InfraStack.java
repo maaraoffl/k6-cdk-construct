4ab4f37b29709b3493365ecd6749162716de0e44
maheshvra <survivewithodds@gmail.com>
2021-07-22 00:40:38 -0400

    first commit

--- src/main/java/com/myorg/K3InfraStack.java
+++ src/main/java/com/myorg/K3InfraStack.java
@@ -3,6 +3,10 @@ package com.myorg;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.core.Tags;
+import software.amazon.awscdk.services.s3.BlockPublicAccess;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.BucketEncryption;
 
 public class K3InfraStack extends Stack {
     public K3InfraStack(final Construct scope, final String id) {
@@ -12,6 +16,15 @@ public class K3InfraStack extends Stack {
     public K3InfraStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        Bucket myBucket = Bucket.Builder.create(this, "myBucket")
+            .bucketName("my-stream-354")
+            .blockPublicAccess(BlockPublicAccess.BLOCK_ALL)
+            .encryption(BucketEncryption.S3_MANAGED)
+            .versioned(true)
+            .build();
+
+        Tags.of(this).add("OwnerContact", "theslidingwindow@gmail.com");
+        System.out.println(myBucket.getBucketArn());
+        
     }
 }
